6cb22a2ed914f920ff1299773368882dd0727505
gianordoli <gianordoligabriel@yahoo.com.br>
2014-03-29 00:19:22 -0400

    Categories as bubble chart
    Creating two tipes of objects: transactions and categories;
    Drawing categories as bubble chart.

--- personal_finance_03_js/public/_activity_sketches/activity_04.js
+++ personal_finance_03_js/public/_activity_sketches/activity_04.js
@@ -39,11 +39,7 @@ var mousePos;
 
 /*--------------- DATA VARIABLES --------------*/
 var activity; //Array; list of all transactions (objects)
-var maxAmount;
-var allDebit;
-var allCredit;
-
-var palette;  //Categories and hues
+var categories;  //Categories and hues
 
 
 /*------------------- EASING ------------------*/
@@ -71,20 +67,12 @@ function setup(data){
     activity.push(transaction);
   }
 
-  maxAmount = getMaxAmount();
-
-  //Spliting the full array into credit and debit
-  allDebit = splitArray('debit');
-  // console.log(allDebit.length);  
-  allCredit = splitArray('credit');
-
   //Create categories colors
-  palette = extractCategories(allDebit);
-  createColors(palette);
+  categories = extractCategories(activity, 'debit');
 
-  //Set color
-  for(var i = 0; i < allDebit.length; i++){
-    allDebit[i].setColor();
+  //Set
+  for(var i = 0; i < categories.length; i++){
+    categories[i].setColor(categories, i);
   }
 
   draw();
@@ -97,24 +85,16 @@ function draw(){
   ctx.strokeStyle = 'black';
   ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-  var pos = { x: 0, y: 0 };
-  var prevMonth = -1;
-  for(var i = allDebit.length - 1; i > 0; i--){
-    pos.x = map(i, 0, allDebit.length - 1, canvas.width, 0);
-    pos.y = map(i, 0, allDebit.length - 1, canvas.height, 0);
-    var drawMonth = false;
-    if(allDebit[i].date.getMonth() != prevMonth){
-      drawMonth = true;
-      prevMonth = allDebit[i].date.getMonth();
-      console.log(prevMonth);
-    };
-    allDebit[i].draw(pos, drawMonth);
+  for(var i = 0; i < categories.length; i++){
+    categories[i].draw();
   }
 
   // request = requestAnimFrame(update);   
 }
 
 /*------------------ OBJECTS ------------------*/
+//TRANSACTIONS
+//------------
 function initTransaction(obj, data){
   //Variables
   obj.description = data.description;
@@ -123,31 +103,41 @@ function initTransaction(obj, data){
   obj.date = new Date(data.date);
   obj.type = data.type;
 
-  obj.setColor = setColorTransaction;
   obj.draw = drawTransaction;
 }
 
-function drawTransaction(pos, drawMonth){
-  // console.log(pos);
-  ctx.fillStyle = this.color;
-  ctx.fillRect(pos.x, 0, canvas.width/allDebit.length, canvas.height);
-  // console.log(this.date.getDate());
-
-  if(drawMonth){
-    ctx.fillStyle = 'black';
-    ctx.font = '400 8px Raleway';
-    ctx.textAlign = 'center';
-    ctx.fillText(this.date.getMonth(), pos.x + 10, pos.y + 10);    
-  }
+function drawTransaction(){
 }
 
-function setColorTransaction(){
-  for(var i = 0; i < palette.length; i++){
-    if (palette[i].category == this.category){
-      this.color = parseHslaColor(palette[i].hue, 100, 50, 0.5);
-      break;
-    };
-  }
+
+//CATEGORIES
+//----------
+function initCategory(obj, description, totalAmount, type){
+  //Variables
+  obj.description = description;
+  obj.totalAmount = totalAmount;
+  obj.type = type;
+
+  obj.setColor = setColorCategory;
+  obj.draw = drawCategory;
+}
+
+function drawCategory(){
+  var pos = { x: Math.random()*canvas.width,
+              y: Math.random()*canvas.height };
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(pos.x, pos.y, this.totalAmount/10, 0, Math.PI*2, false);
+  ctx.fill();
+
+  ctx.fillStyle = 'black';
+  ctx.textAlign = 'center';
+  ctx.fillText(this.description, pos.x, pos.y);
+}
+
+function setColorCategory(myArray, i){
+  var hue = map(i, 0, myArray.length, 0, 360);
+  this.color = parseHslaColor(hue, 100, 50, 0.8);
 }
 
 /*--------------- AUX FUNCTIONS ---------------*/
@@ -167,94 +157,42 @@ function canvasResize(){
 
 
 //Extract categories from a given array
-var extractCategories = function(myArray){
+var extractCategories = function(myArray, filter){
 
   var categoriesList = [];
 
-  //Store the first category
-  var paletteObj = new Object();
-  paletteObj.category = myArray[0].category;
-  categoriesList.push(paletteObj);
-
   //1. Go through the full array of spendings
   for(var i = 0; i < myArray.length; i++){
-    // console.log(myArray[i].category);
-
-    //2. Go through the full array of categories
-    var isStored = false;
-    for(var j = 0; j < categoriesList.length; j++){
-      //If the category is already stored...
-      if(myArray[i].category == categoriesList[j].category){
-        isStored = true;
-        // console.log('exists: ' + myArray[i].category);
-        break;
-      }
-    }
     
-    if(!isStored){
-      var paletteObj = new Object();
-      // console.log('store: ' + myArray[i].category);
-      paletteObj.category = myArray[i].category;
-      // console.log('store: ' + paletteObj.category);
-      categoriesList.push(paletteObj);
+    if(myArray[i].type == filter){
+
+      //2. Go through the full array of categories
+      var isStored = false;
+      for(var j = 0; j < categoriesList.length; j++){
+        //If the category is already stored...
+        if(myArray[i].category == categoriesList[j].description){
+          isStored = true;
+          categoriesList[j].totalAmount += myArray[i].amount;
+          // console.log('exists: ' + myArray[i].category);
+          break;
+        }
+      }
+      
+      if(!isStored){
+        var categoriesObj = new Object();
+        // console.log('store: ' + myArray[i].category);
+        initCategory(categoriesObj, myArray[i].category, myArray[i].amount, filter);
+        categoriesList.push(categoriesObj);
+      }
     }
   }
   
   //Debug
-  // console.log(categoriesList.length);
+  console.log(categoriesList);
   // for(var i = 0; i < categoriesList.length; i++){
   //   console.log('[' + i + ']' + categoriesList[i].category);
   // }
   return categoriesList;
-}
-
-function createColors(myArray){
-  for(var i = 0; i < myArray.length; i++){
-    myArray[i].hue = map(i, 0, myArray.length - 1, 0, 360);
-  }
-
-  //Debug
-  // for(var i = 0; i < myArray.length; i++){
-  //   console.log('hue: ' + myArray[i].hue + ', category: ' + myArray[i].category);
-  // }
-}
-
-var splitArray = function(thisType){
-  var newArray = new Array();
-
-  for(var i = 0; i < activity.length; i++){
-    if(activity[i].type == thisType){
-      newArray.push(activity[i]);
-    }
-  }
-  return newArray;
-}
-
-var getMaxAmount = function(){
-  var max = 0;
-
-  for(var i = 0; i < activity.length; i++){
-    if(activity[i].amount > max){
-      max = activity[i].amount;
-    }
-  }
-  console.log(max);
-  return max;
-}
-
-var daysInBetween = function(date1, date2) {
-    // The number of milliseconds in one day
-    var ONE_DAY = 1000 * 60 * 60 * 24;
-
-    // Convert both dates to milliseconds
-    var date1_ms = date1.getTime();
-    var date2_ms = date2.getTime();
-
-    // Calculate the difference in milliseconds
-    var difference_ms = Math.abs(date1_ms - date2_ms);
-
-    // Convert back to days and return
-    return Math.round(difference_ms/ONE_DAY);
 }
 
 /*--------------- LISTENERS ---------------*/
